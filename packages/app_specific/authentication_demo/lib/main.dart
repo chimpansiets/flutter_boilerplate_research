74696b1c66e520f8a3c13fc211a0046aa15613ae
Sietse <svoort1@live.nl>
2024-02-25 20:36:54 +0100

    add logging

--- packages/app_specific/authentication_demo/lib/main.dart
+++ packages/app_specific/authentication_demo/lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:app_common/app_common.dart';
 import 'package:authentication_demo/handlers/registration_handler.dart';
 import 'package:design_system/design_system.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+
+final log = Logger('MyLogger');
 
 void main() {
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    if (kDebugMode) {
+      print('${record.level.name}: ${record.time}: ${record.message}');
+    }
+  });
+
   runApp(const MyApp());
 }
 
--- packages/data/lib/src/repositories_impl/user_repository_impl.dart
+++ packages/data/lib/src/repositories_impl/user_repository_impl.dart
@@ -1,12 +1,9 @@
 import 'package:entities/entities.dart';
 import 'package:domain/domain.dart';
-import 'package:logging/logging.dart';
 
 class UserRepositoryImpl implements UserRepository {
   @override
   Future<void> registerUser(User user) async {
-    final log = Logger('MyLogger');
-
-    log.info('User registered with ${user.username}, ${user.password}');
+    // Should be an API call to register user or something similar.
   }
 }
