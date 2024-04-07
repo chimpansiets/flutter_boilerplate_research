867e500757dd315f302e9b5bef80f63b47b432e3
Sietse <svoort1@live.nl>
2024-04-07 10:06:37 +0200

    build files

---
+++ packages/design_system_demo_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\svoor\flutter_windows_2.0.5-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=c:\Users\svoor\Documents\Dev\flutterProjects\flutter_boilerplate_research\packages\design_system_demo_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
